501f9d15ce12d8dffb9ae6325f263e58a6da9c9d
Denis Zavedeev <happy96538@gmail.com>
2021-02-27 00:19:24 +0300

    Add tag to log messages.

--- src/main/java/ru/hse/todojavafx/logging/Logger.java
+++ src/main/java/ru/hse/todojavafx/logging/Logger.java
@@ -17,19 +17,19 @@ public final class Logger {
     }
 
     public void trace(String message, Object... arguments) {
-        System.out.println(Instant.now() + " [TRACE] " + String.format(message, arguments));
+        System.out.println(tag + " " + Instant.now() + " [TRACE] " + String.format(message, arguments));
     }
 
     public void debug(String message, Object... arguments) {
-        System.out.println(Instant.now() + " [DEBUG] " + String.format(message, arguments));
+        System.out.println(tag + " " + Instant.now() + " [DEBUG] " + String.format(message, arguments));
     }
 
     public void info(String message, Object... arguments) {
-        System.out.println(Instant.now() + " [INFO] " + String.format(message, arguments));
+        System.out.println(tag + " " + Instant.now() + " [INFO] " + String.format(message, arguments));
     }
 
     public void warn(Throwable throwable, String message, Object... arguments) {
-        System.out.println(Instant.now() + " [WARN] " + String.format(message, arguments));
+        System.out.println(tag + " " + Instant.now() + " [WARN] " + String.format(message, arguments));
         if (throwable != null) {
             throwable.printStackTrace(System.out);
         }
@@ -40,7 +40,7 @@ public final class Logger {
     }
 
     public void error(Throwable throwable, String message, Object... arguments) {
-        System.out.println(Instant.now() + " [ERROR] " + String.format(message, arguments));
+        System.out.println(tag + " " + Instant.now() + " [ERROR] " + String.format(message, arguments));
         if (throwable != null) {
             throwable.printStackTrace(System.out);
         }
